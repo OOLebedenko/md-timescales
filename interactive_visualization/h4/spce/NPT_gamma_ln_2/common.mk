0e3f6cbaa9999c1380246bf668b912f9734fb052
OOLebedenko <oolebedenko@gmail.com>
2019-05-12 17:28:57 +0300

    Add plot_vector_notebook

---
+++ interactive_visualization/h4/spce/NPT_gamma_ln_2/common.mk
@@ -0,0 +1,12 @@
+.EXPORT_ALL_VARIABLES:
+.PHONY: all clean
+
+N_RESIDUES:=$(shell python ${SCRIPT_DIR}/makefile_config.py --n-residues)
+FIRST_CH3_RESIDUES:=$(shell python ${SCRIPT_DIR}/makefile_config.py --first-ch3-residue)
+LAST_CH3_RESIDUES:=$(shell python ${SCRIPT_DIR}/makefile_config.py --last-ch3-residue)
+TRAJECTORY_LENGTH:=100
+TRAJECTORY_PATH:=$(shell python ${SCRIPT_DIR}/makefile_config.py --trajectory-path)
+VOLUME_FILE:=$(shell python ${SCRIPT_DIR}/makefile_config.py --volume-file)
+LATTICE_RST7_FILE:=$(shell python ${SCRIPT_DIR}/makefile_config.py --lattice-rst7-file)
+
+PYTHONPATH:=${SCRIPT_DIR}/..
